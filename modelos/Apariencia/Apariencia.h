e51318b69976b075dfd71ae9fc30a39bbe9d7e22
Andres <andres.fernandezcaballero@davinci.edu.ar>
2021-07-24 17:42:58 -0300

    Eliminadas carpetas y archivos inecesarios

--- modelos/Apariencia/Apariencia.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef APARIENCIA_H
-#define APARIENCIA_H
-
-struct Apariencia {
-	int apariencia; // icono en codigo ASCII que representa al personaje
-	int color; // color del item o personaje;
-};
-#endif // !APARIENCIA_H
